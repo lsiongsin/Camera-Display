27dafde88f5183db4775729df24b39dd86964205
lsiongsin <110996784+lsiongsin@users.noreply.github.com>
2022-08-16 09:41:07 -0400

    Completed Iteration 0

---
+++ Day_7/Camera&Display/Camera&Display/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Camera&Display
+//
+//  Created by Scholar on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+ 
+        
+   
+        
+
+    }
+    
+
+
